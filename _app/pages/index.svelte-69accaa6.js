0154bd03bb8c04888f338d6b9577a5913821d582
alexanderczigler <alexander@czigler.eu>
2022-06-03 22:37:13 +0000

    Deploying to gh-pages from @ alexanderczigler/alexanderczigler@e3e6d079df4995b71d2c4fb16014c796caa4afca 🚀

---
+++ _app/pages/index.svelte-69accaa6.js
@@ -0,0 +1,23 @@
+import{S as B,i as J,s as K,k as d,e as l,t as I,M as Q,d as o,m as h,c as r,a as c,h as b,b as a,N as U,g as N,H as t,n as j}from"../chunks/index-5356398c.js";function W(V){let m,e,u,v,x,p,_,k,i,A,C,f,E,H,g,S,T,y,D,q,w,M;return{c(){m=d(),e=l("div"),u=l("h3"),v=I("Hello!"),x=d(),p=l("p"),_=I("I am Alexander Czigler. I help companies improve their culture and ways of working with code."),k=d(),i=l("img"),C=d(),f=l("p"),E=I(`My story began around 1997 when I got my first PC with dialup internet. I quickly became
+    enchanted by the web and it did not take long until I wanted to build my own homepage so that I
+    could become a part of the web myself. Dialup was expensive I was only allowed to go online for
+    a few hours every week. I spent that precious time online learning about HTML (and playing
+    Flight Simulator 98!) and finding free hosts where I could publish my creations.`),H=d(),g=l("p"),S=I(`In 2003 my journey took a giant leap as I got DSL at home. This was a huge thing for me as I was
+    suddenly able to go online as much as I wanted; I was no longer restricted by the cost or speed
+    of dialup. Having 24/7 access to the internet meant I could spend more time on the social
+    aspects of the internet. I eventually found a bunch of like-minded people on IRC and I started
+    making friends online. Some of the people I got to know taught me about things like linux and
+    running my own servers. They inspired me to build my own computer for the first time, a computer
+    that became my first home server where I installed linux for the first time in my life and I
+    learned how to setup my own web server among other things.`),T=d(),y=l("h3"),D=I("Experience"),q=d(),w=l("p"),M=I("TODO: Add experience"),this.h()},l(s){Q('[data-svelte="svelte-1vtqt6n"]',document.head).forEach(o),m=h(s),e=r(s,"DIV",{class:!0,role:!0});var n=c(e);u=r(n,"H3",{class:!0});var L=c(u);v=b(L,"Hello!"),L.forEach(o),x=h(n),p=r(n,"P",{class:!0});var O=c(p);_=b(O,"I am Alexander Czigler. I help companies improve their culture and ways of working with code."),O.forEach(o),k=h(n),i=r(n,"IMG",{alt:!0,class:!0,src:!0}),C=h(n),f=r(n,"P",{class:!0});var z=c(f);E=b(z,`My story began around 1997 when I got my first PC with dialup internet. I quickly became
+    enchanted by the web and it did not take long until I wanted to build my own homepage so that I
+    could become a part of the web myself. Dialup was expensive I was only allowed to go online for
+    a few hours every week. I spent that precious time online learning about HTML (and playing
+    Flight Simulator 98!) and finding free hosts where I could publish my creations.`),z.forEach(o),H=h(n),g=r(n,"P",{class:!0});var R=c(g);S=b(R,`In 2003 my journey took a giant leap as I got DSL at home. This was a huge thing for me as I was
+    suddenly able to go online as much as I wanted; I was no longer restricted by the cost or speed
+    of dialup. Having 24/7 access to the internet meant I could spend more time on the social
+    aspects of the internet. I eventually found a bunch of like-minded people on IRC and I started
+    making friends online. Some of the people I got to know taught me about things like linux and
+    running my own servers. They inspired me to build my own computer for the first time, a computer
+    that became my first home server where I installed linux for the first time in my life and I
+    learned how to setup my own web server among other things.`),R.forEach(o),T=h(n),y=r(n,"H3",{class:!0});var F=c(y);D=b(F,"Experience"),F.forEach(o),q=h(n),w=r(n,"P",{class:!0});var G=c(w);M=b(G,"TODO: Add experience"),G.forEach(o),n.forEach(o),this.h()},h(){document.title="Alexander Czigler",a(u,"class","font-firaCode my-4 text-2xl"),a(p,"class","my-4"),a(i,"alt","Headstand"),a(i,"class","border-slate-400 rounded-lg"),U(i.src,A="hero.jpg")||a(i,"src",A),a(f,"class","my-4"),a(g,"class","my-4"),a(y,"class","font-firaCode my-4 text-2xl"),a(w,"class","my-4"),a(e,"class","border-t-background-fade border-t flex flex-col justify-center items-left font-josefinSans px-5 pt-5 font-light text-text"),a(e,"role","main")},m(s,P){N(s,m,P),N(s,e,P),t(e,u),t(u,v),t(e,x),t(e,p),t(p,_),t(e,k),t(e,i),t(e,C),t(e,f),t(f,E),t(e,H),t(e,g),t(g,S),t(e,T),t(e,y),t(y,D),t(e,q),t(e,w),t(w,M)},p:j,i:j,o:j,d(s){s&&o(m),s&&o(e)}}}class Y extends B{constructor(m){super(),J(this,m,null,W,K,{})}}export{Y as default};
